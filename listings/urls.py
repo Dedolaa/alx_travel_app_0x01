ac8a7eb0a34bf30abfc1931c7413b55255bea395
Dedolaa <adedolapobakare16@gmail.com>
2025-07-31 17:30:39 +0100

    Add files via upload

---
+++ listings/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
+from . import views
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
